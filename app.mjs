5816aa16eab41093203c04ac4e9ecf7690897d6d
David Fernando Ortega <davidoc96@hotmail.com>
2025-03-07 11:29:55 +0100

    Lab session 20250307

--- app.mjs
+++ app.mjs
@@ -1,1 +1,32 @@
-console.log('This is a test')
+"use strict";
+
+const validIngredients = ["avocado", "ananas", "cashew nuts", "kale", "mango", "peppers", "corn", 
+    "wakame", "tomatoes", "carrots", "salad"];
+const validProteins = ['Tuna', 'Chicken', 'Salmon', 'Tofu']
+const validBases = ['Rice', 'Black rice', 'Salad']
+
+function Bowl(size, base){
+    this.size = size;
+    this.base = (base in validBases) && base;
+    this.ingredients = [];
+    this.proteins = [];
+    this.price = undefined;
+
+    this.addIngredient = (ingr) =>{
+        // add check if(ingr in validIngredients)
+        this.ingredients.push(ingr)
+
+    }
+}
+
+
+function Order(){
+    this.bowls = [];
+    this.discount = 0;
+}
+
+
+let a = Bowl('R', 'Rice');
+
+
+
