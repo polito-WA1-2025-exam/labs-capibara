7868978bf97a97bda3588993f3e241a69ef81ce0
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-03-09 17:36:26 +0100

    Add input functions and validation for Bowl size, base, ingredients, and proteins; also added some example data for testing purposes.

--- app.mjs
+++ app.mjs
@@ -1,5 +1,7 @@
 "use strict";
 
+const validSizes = ['R', 'M', 'L'];
+
 const validIngredients = ["avocado", "ananas", "cashew nuts", "kale", "mango", "peppers", "corn", 
     "wakame", "tomatoes", "carrots", "salad"];
 const validProteins = ['Tuna', 'Chicken', 'Salmon', 'Tofu']
@@ -7,14 +9,40 @@ const validBases = ['Rice', 'Black rice', 'Salad']
 
 function Bowl(size, base){
     this.size = size;
-    this.base = (base in validBases) && base;
+    this.base = base;
     this.ingredients = [];
     this.proteins = [];
     this.price = undefined;
 
+    // check if the size is valid
+    if(!this.size || !(validSizes.includes(size))){
+        console.log("Invalid size: " + size);
+        this.size = undefined;
+    }
+
+    // check if the base is valid
+    if(!this.base || !(validBases.includes(base))){
+        console.log("Invalid base: " + base);
+        this.base = undefined;
+    }
+
     this.addIngredient = (ingr) =>{
-        // add check if(ingr in validIngredients)
-        this.ingredients.push(ingr)
+        // check if the ingredient is valid
+        // if it is, add it to the ingredients
+        if(validIngredients.includes(ingr))
+            this.ingredients.push(ingr);
+        else
+            console.log("Invalid ingredient: " + ingr);
+
+    }
+
+    this.addProtein = (protein) =>{
+        // check if the protein is valid 
+        // if it is, add it to the proteins array
+        if(validProteins.includes(protein))
+            this.proteins.push(protein);
+        else
+            console.log("Invalid protein: " + protein);
 
     }
 }
@@ -26,7 +54,20 @@ function Order(){
 }
 
 
-let a = Bowl('R', 'Rice');
+let a = new Bowl('R', 'Rice');
+
+// Example of ingredients and proteins insertion
+a.addIngredient('avocado');
+a.addIngredient('ananas');
+a.addProtein('Tuna');
+a.addProtein('Chicken');
+
+console.log(a);
+console.log("ingredients: " + a.ingredients);
+console.log("proteins: " + a.proteins);
+
+// Example of invalid ingredient
+a.addIngredient('banana');
 
 
 
