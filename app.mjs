61fb1d4a69c6f59ea50155c42fc87fd161930ad4
David Fernando Ortega <davidoc96@hotmail.com>
2025-03-14 11:28:52 +0100

    Adding Database file and update_price

--- app.mjs
+++ app.mjs
@@ -6,6 +6,24 @@ const validIngredients = ["avocado", "ananas", "cashew nuts", "kale", "mango", "
     "wakame", "tomatoes", "carrots", "salad"];
 const validProteins = ['Tuna', 'Chicken', 'Salmon', 'Tofu']
 const validBases = ['Rice', 'Black rice', 'Salad']
+const bowl_sizes ={
+    'R' : {
+        'base_price' : 9
+        , 'max_ingredients' : 4
+        , 'protein_num': 1
+    }
+    , 'M' : {
+        'base_price' : 11
+        , 'max_ingredients' : 4
+        , 'protein_num': 2
+    }
+    , 'L' : {
+        'base_price' : 14
+        , 'max_ingredients' : 6
+        , 'protein_num': 3
+    }
+}
+
 
 function Bowl(size, base){
     this.size = size;
@@ -13,6 +31,7 @@ function Bowl(size, base){
     this.ingredients = [];
     this.proteins = [];
     this.price = undefined;
+    
 
     // check if the size is valid
     if(!this.size || !(validSizes.includes(size))){
@@ -25,6 +44,9 @@ function Bowl(size, base){
         console.log("Invalid base: " + base);
         this.base = undefined;
     }
+
+    this.price = bowl_sizes[this.size].base_price
+
 
     this.addIngredient = (ingr) =>{
         // check if the ingredient is valid
@@ -43,6 +65,17 @@ function Bowl(size, base){
             this.proteins.push(protein);
         else
             console.log("Invalid protein: " + protein);
+
+    }
+
+    this.updatePrice = () => {
+        tot_ingredients = this.ingredients.length
+        max_ingredients = bowl_sizes[this.size].max_ingredients
+
+        if (tot_ingredients > max_ingredients) {
+            extra_ingredients = tot_ingredients - max_ingredients
+            this.price = this.price * (1 + .2 * extra_ingredients)
+        }
 
     }
 }
